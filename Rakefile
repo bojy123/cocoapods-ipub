a010a740809e5ba78a252f8d6dac2d3468229c90
tl <tl@iMac.local>
2018-09-19 21:47:26 +0800

    a

--- Rakefile
+++ Rakefile
@@ -5,8 +5,8 @@ def specs(dir)
 end
 
 def build
-	sh('gem build cocoapods-develop.gemspec')
-	sh('gem install cocoapods-develop-0.0.1.gem')
+	sh('gem build cocoapods-ipub.gemspec')
+	sh('sudo gem install cocoapods-ipub-0.0.1.gem')
 end
 
 desc 'Builds the gem'
--- lib/cocoapods-ipub/pod_ipub.rb
+++ lib/cocoapods-ipub/pod_ipub.rb
@@ -6,9 +6,21 @@ module Pod
     module DSL
 
       def pod_ipub(name = nil, *requirements)
-        pod(name, *requirements)
+        r = requirements
+        r.push(:ipub => true)
+        pod(name, r)
       end
 
     end
   end
+
+
+  class Dependency
+    def is_ipub?
+      if external_source
+        external_source[:ipub]
+      end
+    end
+  end
+
 end
